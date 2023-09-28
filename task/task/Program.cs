9f6c1c97dcf5a9c45e0fdc7d2d722a4b425da59c
shito.bulo@gmail.com <shito.bulo@gmail.com>
2023-09-29 01:22:24 +0300

    Created LastSeenUsers class

--- task/task/Program.cs
+++ task/task/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
+
+public class LastSeenUsers
+{
+    public string userId;
+    public string nickname;
+    public string firstName;
+    public string lastName;
+    public DateTime? registrationDate;
+    public DateTime? lastSeenDate;
+    public bool isOnline;
+};
